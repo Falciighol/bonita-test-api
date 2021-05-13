ad1b7768c05dab771ecaf40da6d47b50f3aa14cf
Falcighol <juampa.hernandez@outlook.com>
2021-05-13 16:50:59 -0600

    Update project information

--- src/main/java/com/bird/connection/Connect.java
+++ src/main/java/com/bird/connection/Connect.java
@@ -12,16 +12,19 @@ import org.slf4j.LoggerFactory;
 
 import org.bonitasoft.web.extension.ResourceProvider;
 
-public class Connect {
+public class Connect
+{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Connect.class);
-
-    public static Connection connect(ResourceProvider pageResourceProvider) throws NamingException {
+    
+    public static Connection connect(ResourceProvider pageResourceProvider) throws NamingException
+    {
         Connection conn = null;
         Context ctx = new InitialContext();
 
         try {
             DataSource dataSource = (DataSource) ctx.lookup("java:comp/env/demoDS");
+            // DataSource dataSource = (DataSource) ctx.lookup(getDSName());
             conn = (Connection) dataSource.getConnection();
         } catch (Exception e) {
             LOGGER.error("Error while connecting to DB");
@@ -30,5 +33,20 @@ public class Connect {
         return conn;
 	}
 
+    public static Connection connect(ResourceProvider pageResourceProvider, String DSName) throws NamingException
+    {
+        Connection conn = null;
+        Context ctx = new InitialContext();
+
+        try {
+            DataSource dataSource = (DataSource) ctx.lookup("java:comp/env/" + DSName);
+            // DataSource dataSource = (DataSource) ctx.lookup(getDSName());
+            conn = (Connection) dataSource.getConnection();
+        } catch (Exception e) {
+            LOGGER.error("Error while connecting to DB");
+            LOGGER.error("Message: " + e.getMessage());
+        }
+        return conn;
+	}
 
 }
